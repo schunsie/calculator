09a7f3f28d48e01257a82629f06c0ca728793b7e
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-18 17:28:08 +0100

    Add main operate function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let number1;
+let operator;
+let number2;
+
+function operate(num1, op, num2) {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'ERROR'
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
