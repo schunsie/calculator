75146d2a6880be9c026610fcc6424d517a2e171b
schunsie <185822585+schunsie@users.noreply.github.com>
2024-11-19 11:19:39 +0100

    Add feature to populate screen with digits
    Clicking a button will populate the screen with it's contents.
    Will require some more sanitizing of input. Also, operator handling
    is still needed.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const calc = document.querySelector('.calc-result');
+const keypad = document.querySelector('.keypad');
+
+keypad.addEventListener('click', (event) => {
+    const target = event.target;
+    if (Array.from(target.classList).includes('keypad')) {
+        return;
+    }
+
+    populateScreen(target.textContent);
+});
+
+function populateScreen(number) {
+    calc.textContent += number;
+}
+
+
 let number1;
 let operator;
 let number2;
